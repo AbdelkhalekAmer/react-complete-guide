c01a6bacb768a35be88e00cee5f6342caa0dd1c8
Abdelkhalek <abdelkhalekamer@outlook.com>
2021-08-15 18:51:43 +0200

    Add card component and update folders structure

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,13 @@
 import './Expenses.css';
+import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 
 function Expenses(props) {
     const expenses = props.expenses;
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             {expenses.map(item => (<ExpenseItem date={item.date} title={item.title} amount={item.amount} />))}
-        </div>
+        </Card>
     );
 }
 
